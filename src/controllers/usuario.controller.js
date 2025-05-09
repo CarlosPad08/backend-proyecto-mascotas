a3efc5385bbd010d2bfb8878fc41b224925d7524
Carlos Padilla <101229982+CarlosPad08@users.noreply.github.com>
2025-05-09 18:39:06 -0500

    Update usuario.controller.js
    Se cambian permisos para habilitar cookies en hosting (render.com)

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -28,8 +28,8 @@ export const iniciarSesion = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 12 * 60 * 60 * 1000,
     });
     
